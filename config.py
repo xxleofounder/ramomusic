7600de08351ffd465c15be83d61c11f84c9fab0c
xxleofounder <ahmetnurullah033@gmail.com>
2025-09-14 13:38:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ if os.path.exists("local.env"):
 
 load_dotenv()
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
+SESSION_NAME = getenv("SESSION_NAME", "BAFN6r0As-zf7RjX9rV9mx-FpqRb6m1mIzW8GBFZ-0lH-rfOsyjUOLjF6AcExzhOdeaDf1CGh8ljBH2j169S3ujwrKYFSztRBg4dS2kfjJWU25M2sPC9Jk_P5l-ybuoKDYGjdt7tEVpqlrhMlCmIZ4-YNVqESuM8DeKaLXh4_PmRz0SrBWdsBkCfYnG4IogFu49e4Ej-bEZ8rQBPpvDicnIpd8JjUl6t98BDPO0mqlBUsWY54wuI534tJKln8iJvDa-HsDuHuUmOgN37EtWgI4YP5HQX1MYWQZCa1c_URR-rOKgHJJBLIsYZEL2NCoQnVTzNkH-vShLXeys3X2Aoc2ZbEpvjmAAAAAHnAcMFAA")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME", "Talia Müzik Projesi")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/cd0b87484429704c7b935.png")
@@ -15,10 +15,10 @@ THUMB_IMG = getenv("THUMB_IMG", "https://i.ibb.co/VQd4HSH/Photo-1629477903701.jp
 AUD_IMG = getenv("AUD_IMG", "https://i.ibb.co/VQd4HSH/Photo-1629477903701.jpg")
 QUE_IMG = getenv("QUE_IMG", "https://i.ibb.co/VQd4HSH/Photo-1629477903701.jpg")
 admins = {}
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "Mp3dinleme_Bot ")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "SesMusicAsistan")
+API_ID = int(getenv("21883581"))
+API_HASH = getenv("c3b4ba58d5dada9bc8ce6c66e09f3f12")
+BOT_USERNAME = getenv("BOT_USERNAME", "leousertaggerbot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "ramomusicasis")
 GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Sohbetdestek")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "EfsaneMusicProject")
 OWNER_NAME = getenv("OWNER_NAME", "Mahoaga") # kullanıcı adınızı semboller olmadan doldurma @
